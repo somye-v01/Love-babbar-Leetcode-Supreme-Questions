5a7c0075b29ecc30fea63194b7956dd9dcedc358
Somyeh Virmani <somye01dev@gmail.com>
2023-06-04 19:33:53 +0530

    basic questions

---
+++ recursion/find max element in arr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<climits>
+using namespace std;
+int maxi = INT_MIN;
+int findMax(int arr[], int n, int i){
+    if(i==n){
+        return INT_MIN;
+    }
+    
+    if(arr[i]>maxi){
+         maxi = arr[i];
+    }
+    findMax(arr,n,i+1);
+    return maxi;
+    
+}
+
+int main(){
+    int arr[6] = {10,2,15,20,25,14};
+    int n = 6;
+    int i = 0;
+    int max_element = findMax(arr,n,i);
+    cout<<max_element<<endl;
+}
---
+++ recursion/print all numbers in arr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void print(int arr[], int n, int i) {
+//base case
+if( i >= n) {
+return;
+}
+
+//1 case solve krdia
+cout << arr[i] << " ";
+//baaki recursion sambhal lega
+print( arr,  n, i+1);
+
+
+}
+
+int main() {
+int arr[5] = {10,20,30,40,50};
+int n = 5;
+int i = 0;
+print(arr, n, i);
+}
