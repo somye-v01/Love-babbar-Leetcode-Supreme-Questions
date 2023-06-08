697afc3b420a4d13fe9d9ce074c5ca5b6e022661
Somyeh Virmani <somye01dev@gmail.com>
2023-06-08 23:47:43 +0530

    a basic question, 1trick.
    how to call if for all the i's is the trick. so solve for one using recursion, call that function in a loop for different values of start in another function.

---
+++ recursion/print all subarrays.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void printallsubarrays(vector<int>& v, int start, int end){
+    if(end==v.size()){
+        return;
+    }
+    for(int i = start;i<=end;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    printallsubarrays(v,start,end+1);
+}
+void callforallstarts(vector<int>& v, int start, int end){
+    for(int i = 0; i<v.size(); i++){
+        end =i;
+        printallsubarrays( v, i,  end);
+    }
+}
+
+int main() {
+    // Write C++ code here
+    vector<int> v = {1,2,3,4,5};
+    callforallstarts(v,0,0);
+
+    return 0;
+}
